1f0d90f3781b397a3276cd764557389e704f17e2
Olmo <olmot.s@gmail.com>
2018-11-27 10:14:45 +0100

    Nueva versión incompleta

--- Retratos-Robot/src/Main.java
+++ Retratos-Robot/src/Main.java
@@ -2,132 +2,34 @@ import java.util.Scanner;
 
 public class Main {
 	
-	static void retratos() {
+	
+	static String [][] rasgos = {
+								{"WWWWWWWWW" ,"\\\\\\//////" ,"|\"\"\"\"\"\"\"|" , "|||||||||"} ,
+								{ "|  O O  |" , " |-(· ·)-|" , "|-(o o)-|" , "|  \\ /  |" },
+								{" @   J   @" , "{   \"   }" , "[   j   ]" , "<   -   >" }, 
+								{ "|  ===  |" , "|   -   |" , "|  ___  |" , "|  ---  |" } ,
+								{ "\\_______/ " , "\\,,,,,,,/"}
+};
+	
+
+	static String grtOpcion(int rasgo, String nombre) {
 		Scanner teclado = new Scanner(System.in);
-		int opcionPelo;
+		int opcion;
+		for(int i = 0; i < rasgos[rasgo].length; i++) {
+		System.out.println(rasgos[rasgo][i]);
 		do {
-			System.out.println("Selecciona el pelo(1-4): ");
-			System.out.println("1. WWWWWWWWW ");
-			System.out.println("2. \\\\\\//////");
-			System.out.println("3. |\"\"\"\"\"\"\"|");
-			System.out.println("4. |||||||||");
-
-			opcionPelo = teclado.nextInt();
-			if (opcionPelo > 4 || opcionPelo < 1) {
-				System.out.println("opción no válida para pelo, vuelve a introducir");
-			}
-		} while (opcionPelo > 4 || opcionPelo < 1);
-
-		int opcionOjos;
-		do {
-			System.out.println("Selecciona los ojos(1-4): ");
-			System.out.println("1. |  O O  |");
-			System.out.println("2. |-(· ·)-|");
-			System.out.println("3. |-(o o)-|");
-			System.out.println("4. |  \\ /  |");
-
-			opcionOjos = teclado.nextInt();
-			if (opcionOjos > 4 || opcionOjos < 1) {
-				System.out.println("opción no válida para ojos, vuelve a introducir");
-			}
-		} while (opcionOjos > 4 || opcionOjos < 1);
-
-		int opcionNariz;
-
-		do {
-			System.out.println("Selecciona la Nariz y Orejas(1-4): ");
-			System.out.println("1. @   J   @");
-			System.out.println("2. {   \"   }");
-			System.out.println("3. [   j   ]");
-			System.out.println("4. <   -   >");
-
-			opcionNariz = teclado.nextInt();
-			if (opcionNariz > 4 || opcionNariz < 1) {
-				System.out.println("opción no válida para Nariz, vuelve a introducir");
-			}
-		} while (opcionNariz > 4 || opcionNariz < 1);
-
-		int opcionBoca;
-
-		do {
-			System.out.println("Selecciona la Boca(1-4): ");
-
-			System.out.println("1. |  ===  |");
-			System.out.println("2. |   -   |");
-			System.out.println("3. |  ___  |");
-			System.out.println("4. |  ---  |");
-
-			opcionBoca = teclado.nextInt();
-			if (opcionBoca > 4 || opcionBoca < 1) {
-				System.out.println("opción no válida para Boca, vuelve a introducir");
-			}
-		} while (opcionBoca > 4 || opcionBoca < 1);
-
-		switch (opcionPelo) {
-		case 1:
-			System.out.println("WWWWWWWWW");
-			break;
-		case 2:
-			System.out.println("\\\\\\//////");
-			break;
-		case 3:
-			System.out.println("|\"\"\"\"\"\"\"|");
-			break;
-		case 4:
-			System.out.println("|||||||||");
-
-		}
-		switch (opcionOjos) {
-		case 1:
-			System.out.println("|  O O  |");
-			break;
-		case 2:
-			System.out.println("|-(· ·)-|");
-			break;
-		case 3:
-			System.out.println("|-(o o)-|");
-			break;
-		case 4:
-			System.out.println("|  \\\\ /  |");
-
-		}
-
-		switch (opcionNariz) {
-		case 1:
-			System.out.println("@   J   @");
-			break;
-		case 2:
-			System.out.println("{   \"   }");
-			break;
-		case 3:
-			System.out.println("[   j   ]");
-			break;
-		case 4:
-			System.out.println(" <   -   >");
-
-		}
-
-		switch (opcionBoca) {
-		case 1:
-			System.out.println("|  ===  |");
-			break;
-		case 2:
-			System.out.println("|   -   |");
-			break;
-		case 3:
-			System.out.println("|  ___  |");
-			break;
-		case 4:
-			System.out.println("|  ---  |");
-		}
-		
-		System.out.println("\\_______/ ");
-		
-		System.out.println("  WANTED   ");
+			System.out.println("Elige opción para: " + nombre + "(1- " + rasgo);
+			opcion = teclado.nextInt();
+			if(opcion < 1 || opcion > rasgos[rasgo].length)
+				System.out.println("La opción elegida no es correcta. vuelve a elegir: ");
+		}while (opcion < 1 || opcion > rasgos [rasgo].length);
+		return rasgos [rasgo][opcion-1];
 	}
-
+	
+	
+	
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
+		
 		int n;
 		do {
     	retratos();
@@ -135,6 +37,145 @@ public class Main {
     	n = teclado.nextInt();
     }while(n!=0);
      }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	static void retratos() {
+//		Scanner teclado = new Scanner(System.in);
+//		int opcionPelo;
+//		do {
+//			System.out.println("Selecciona el pelo(1-4): ");
+//			System.out.println("1. WWWWWWWWW ");
+//			System.out.println("2. \\\\\\//////");
+//			System.out.println("3. |\"\"\"\"\"\"\"|");
+//			System.out.println("4. |||||||||");
+//
+//			opcionPelo = teclado.nextInt();
+//			if (opcionPelo > 4 || opcionPelo < 1) {
+//				System.out.println("opción no válida para pelo, vuelve a introducir");
+//			}
+//		} while (opcionPelo > 4 || opcionPelo < 1);
+//
+//		int opcionOjos;
+//		do {
+//			System.out.println("Selecciona los ojos(1-4): ");
+//			System.out.println("1. |  O O  |");
+//			System.out.println("2. |-(· ·)-|");
+//			System.out.println("3. |-(o o)-|");
+//			System.out.println("4. |  \\ /  |");
+//
+//			opcionOjos = teclado.nextInt();
+//			if (opcionOjos > 4 || opcionOjos < 1) {
+//				System.out.println("opción no válida para ojos, vuelve a introducir");
+//			}
+//		} while (opcionOjos > 4 || opcionOjos < 1);
+//
+//		int opcionNariz;
+//
+//		do {
+//			System.out.println("Selecciona la Nariz y Orejas(1-4): ");
+//			System.out.println("1. @   J   @");
+//			System.out.println("2. {   \"   }");
+//			System.out.println("3. [   j   ]");
+//			System.out.println("4. <   -   >");
+//
+//			opcionNariz = teclado.nextInt();
+//			if (opcionNariz > 4 || opcionNariz < 1) {
+//				System.out.println("opción no válida para Nariz, vuelve a introducir");
+//			}
+//		} while (opcionNariz > 4 || opcionNariz < 1);
+//
+//		int opcionBoca;
+//
+//		do {
+//			System.out.println("Selecciona la Boca(1-4): ");
+//
+//			System.out.println("1. |  ===  |");
+//			System.out.println("2. |   -   |");
+//			System.out.println("3. |  ___  |");
+//			System.out.println("4. |  ---  |");
+//
+//			opcionBoca = teclado.nextInt();
+//			if (opcionBoca > 4 || opcionBoca < 1) {
+//				System.out.println("opción no válida para Boca, vuelve a introducir");
+//			}
+//		} while (opcionBoca > 4 || opcionBoca < 1);
+//
+//		switch (opcionPelo) {
+//		case 1:
+//			System.out.println("WWWWWWWWW");
+//			break;
+//		case 2:
+//			System.out.println("\\\\\\//////");
+//			break;
+//		case 3:
+//			System.out.println("|\"\"\"\"\"\"\"|");
+//			break;
+//		case 4:
+//			System.out.println("|||||||||");
+//
+//		}
+//		switch (opcionOjos) {
+//		case 1:
+//			System.out.println("|  O O  |");
+//			break;
+//		case 2:
+//			System.out.println("|-(· ·)-|");
+//			break;
+//		case 3:
+//			System.out.println("|-(o o)-|");
+//			break;
+//		case 4:
+//			System.out.println("|  \\\\ /  |");
+//
+//		}
+//
+//		switch (opcionNariz) {
+//		case 1:
+//			System.out.println("@   J   @");
+//			break;
+//		case 2:
+//			System.out.println("{   \"   }");
+//			break;
+//		case 3:
+//			System.out.println("[   j   ]");
+//			break;
+//		case 4:
+//			System.out.println("<   -   >");
+//
+//		}
+//
+//		switch (opcionBoca) {
+//		case 1:
+//			System.out.println("|  ===  |");
+//			break;
+//		case 2:
+//			System.out.println("|   -   |");
+//			break;
+//		case 3:
+//			System.out.println("|  ___  |");
+//			break;
+//		case 4:
+//			System.out.println("|  ---  |");
+//		}
+//		
+//		System.out.println("\\_______/ ");
+//		
+//		System.out.println("  WANTED   ");
+	}
+
+	
+	
 	}
 
 
